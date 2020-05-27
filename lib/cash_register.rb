57ebe6e5b7c024fbae9e1a9ae2ca1f9a66ce7dc7
Ian Hutson <ih00209@georgiasouthern.edu>
2020-05-27 18:21:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,11 +18,12 @@ class CashRegister
     self.last_transaction = price * quantity
   end
   
-  def apply_discount
-    if discount = 0
-      puts "There is no discount to apply."
+   def apply_discount
+    if discount != 0
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+      "After the discount, the total comes to $#{self.total}."
     else
-      self.total = (discount / 100).to_f
+      "There is no discount to apply."
     end
   end
 
